be5e3e9ccdb9c7957decf855e4d8ad182cfd9120
uruzbidiwala <bidiwaur@mail.uc.edu>
2025-02-26 20:13:30 -0500

    Customer

---
+++ JudiciousJays_Assignment06/customer_package/customer.py
@@ -0,0 +1,46 @@
+# File Name : customer.py
+# Student Name: Uruz Bidiwala
+# email:  bidiwaur@mail.uc.edu
+# Assignment Number: Assignment 06
+# Due Date:   Feb 27, 2025
+# Course #/Section: IS 4010-001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment: Group assignment
+
+# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+# Citations: {"Stack Overflow" is not sufficient. Provide repeatable links, book page #, etc.}
+
+# Anything else that's relevant:
+
+class Customer:
+    def __init__(self, name, license_number):
+        self._name = name
+        self._license_number = license_number
+
+    def __str__(self):
+        return f"Customer: {self._name}, License: {self._license_number}"
+
+    def __repr__(self):
+        return f"Customer('{self._name}', '{self._license_number}')"
+
+    # Property for name with getter and setter
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
+
+    # Property for license_number with getter and setter
+    @property
+    def license_number(self):
+        return self._license_number
+
+    @license_number.setter
+    def license_number(self, new_license):
+        self._license_number = new_license
+
+    # A method to greet the customer
+    def greet(self):
+        return f"Hello, my name is {self._name} and I'm ready to rent a car!"
