8994539f20627e75afcdf0b7e8be33c9e31e58a0
Peter Phan <petervphan03@gmail.com>
2025-02-26 19:51:11 -0500

    Add project files.

---
+++ JudiciousJays_Assignment06/mainPackage/main.py
@@ -0,0 +1,15 @@
+# File Name : main.py
+# Student Name: Peter Phan, Uruz Bidiwala, Zoha Iqbal, Saivamsi Reddy Amireddy
+# email:  phanpv@mail.uc.edu, bidiwaur@mail.uc.edu, iqbalza@mail.uc.edu, amiredsr@mail.uc.edu
+# Assignment Number: Assignment 06
+# Due Date:   2/27/2025
+# Course #/Section:   IS4010-001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  Modeling something in the real world using classes
+
+# Brief Description of what this module does. Demonstrate knowledge on Object-Oriented Programming
+# Citations: 
+# Anything else that's relevant:
+
+if __name__ == "__main__":
+    pass
